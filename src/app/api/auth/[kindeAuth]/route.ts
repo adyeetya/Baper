846e2eeb963e95e7e1e484395bcf4edd84b6b607
adyeetya <2612adityasingh2000@gmail.com>
2023-12-03 03:40:35 +0530

    diff auth route

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,7 +1,14 @@
+// import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
+// import { NextRequest } from 'next/server'
+
+// export async function GET(request: NextRequest, { params }: any) {
+//   const endpoint = params.kindeAuth
+//   return handleAuth(request, endpoint)
+// }
+
 import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
 import { NextRequest } from 'next/server'
-
-export function GET(request: NextRequest, { params }: any) {
+export async function GET(request: NextRequest, { params }: any) {
   const endpoint = params.kindeAuth
-  return handleAuth(request, endpoint)
+  return await handleAuth(request, endpoint)
 }
