a8619ce30be539cecd9d3db34be5d02f006d81fa
adyeetya <2612adityasingh2000@gmail.com>
2023-12-03 02:34:06 +0530

    mobile navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { cn } from '../lib/utils'
+import { cn, constructMetadata } from '../lib/utils'
 import Navbar from '../components/Navbar'
 import Providers from '../components/Providers'
 import 'react-loading-skeleton/dist/skeleton.css'
@@ -15,10 +15,7 @@ import 'simplebar-react/dist/simplebar.min.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'baper',
-  description: "Upload PDF's and ask questions.",
-}
+export const metadata= constructMetadata()
 
 export default function RootLayout({
   children,
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,6 +9,7 @@ import {
 } from '@kinde-oss/kinde-auth-nextjs/server'
 import { ArrowRight } from 'lucide-react'
 import UserAccountNav from './UserAccountNav'
+import MobileNav from './MobileNav'
 
 const Navbar = async () => {
   const { getUser } = getKindeServerSession()
@@ -28,7 +29,7 @@ const Navbar = async () => {
             </span>
           </Link>
 
-          {/* todo- add mobile navbar */}
+          <MobileNav isAuth={!!user} />
 
           <div className="hidden items-center space-x-4 sm:flex">
             {!user ? (
